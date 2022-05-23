defa4c1148bfe41a591b3df0082c150432341aa6
toto <dsklpsdj@gmail.com>
2022-05-23 14:30:41 +0200

    fghfg

---
+++ app/src/main/java/com/example/lecoledesloustics/CreateAccount.java
@@ -0,0 +1,14 @@
+package com.example.lecoledesloustics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+    }
+}
---
+++ app/src/main/java/com/example/lecoledesloustics/db/User.java
@@ -0,0 +1,27 @@
+package com.example.lecoledesloustics.db;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name="name")
+    private String name;
+
+
+    public int getId() { return id; }
+
+    public void setId(int id) {this.id = id;}
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name=name; }
+
+}
