4163487a7d79f55ba1909899495242fdb73636e6
Baptiste380 <baptiste38.nicolet@orange.fr>
2022-05-23 13:22:38 +0200

    First Commit

---
+++ app/src/main/java/com/example/lecoledesloustics/Accueil.java
@@ -0,0 +1,13 @@
+package com.example.lecoledesloustics;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+
+public class Accueil {
+
+
+}
