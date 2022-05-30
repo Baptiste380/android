abacf8d522e076fcae8ac3fcf236938ac26e5c7d
Baptiste380 <baptiste38.nicolet@orange.fr>
2022-05-30 13:44:31 +0200

    First Commit

--- app/src/main/java/com/example/lecoledesloustics/Accueil.java
+++ app/src/main/java/com/example/lecoledesloustics/Accueil.java
@@ -8,6 +8,27 @@ import android.widget.TextView;
 import android.widget.RelativeLayout;
 
 public class Accueil {
+    btninsert = (Button)findViewById(R.id.createButton);
+    btninsert.setOnClickListener( new View.OnClickListener() {
+        public void onClick(View v) {
+            insertValues();
+            EditText userName = (EditText) findViewById(R.id.editText1);
 
+            if( userName.getText().toString().length() == 0 )
+                userName.setError( "First name is required!" );
+
+            Intent i = new Intent(getApplicationContext(), Login.class);
+            startActivity(i);
+        }
+    });
 
 }
+    public void bouton(View view) {
+        // On récupére les objets de l'arbre graphique (à l'aide de leur id)
+        TextView helloView = findViewById(R.id.exercice1_hello);
+        EditText prenomView = findViewById(R.id.exercice1_prenom);
+// On affiche le message si un prénom a été défini
+        if (!TextUtils.isEmpty(prenomView.getText())) {
+            helloView.setText("Hello " +prenomView.getText());
+        }
+    }
